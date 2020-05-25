fbdee278597e443415b34ccd4e1dba40b5718530
jacklisp <jacklisp@gmail.com>
2020-05-25 08:41:01 +0800

    t

---
+++ task-4/src/com/example/task_4/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.task_4;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.view.Menu;
+import android.widget.Button;
+import android.view.*;
+import android.widget.*;
+
+public class MainActivity extends Activity {
+	Button btn1;
+	Button btn2;
+	Button btn3;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        
+        btn1 = (Button)findViewById(R.id.button1);
+        btn2 = (Button)findViewById(R.id.button2);
+        btn3 = (Button)findViewById(R.id.button3);
+        
+        //增加btn1,btn2,btn3的功能
+        
+        
+        
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+    
+}
